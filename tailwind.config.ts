6b9bcea578508b1d9a49c162253368dd194634ea
Payman <payman.hoseini7@gmail.com>
2024-09-12 22:13:13 +0430

    config dark and light mode

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,18 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        'Lime-Green' : 'hsl(163, 72%, 41%)',
+        'Bright-Red' : 'hsl(356, 69%, 56%)',
+        'Facebook' : 'hsl(208, 92%, 53%)',
+        'Twitter' : 'hsl(203, 89%, 53%)',
+        'Instagram_1' : 'hsl(37, 97%, 70%)',
+        'Instagram_2' : 'hsl(329, 70%, 58%)',
+        'YouTube': 'hsl(348, 97%, 39%)',
+        'background' : 'var(--background)',
+        'top-background' : 'var(--top-background)',
+        'card': 'var(--card)',
+        'text' : 'var(--text)',
+        'bw-text': 'var(--bw-text)',
       },
     },
   },
