f7b070dfec77e0e63d028185a88fb2c0233f4b4e
Payman <payman.hoseini7@gmail.com>
2024-09-12 23:16:17 +0430

    create header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,21 @@ export default function Home() {
   return (
     <>
     <div className="light">
-      <p className="bg-background">Payman</p>
+        <div className="max-w-[1440px] mx-auto">
+            <div className="flex justify-between items-center py-7">
+              <div>
+                <h1 className="font-bold text-3xl text-bw-text">Social Media Dashboard</h1>
+                <p className="text-text font-bold">Total Followers: 23,004</p>
+              </div>
+              <div className="flex items-center space-x-5">
+                <p className="text-text font-bold text-sm">Dark Mode</p>
+                <label className="switch">
+                    <input type="checkbox" />
+                    <span className="slider round"></span>
+                </label>
+              </div>
+            </div>
+        </div>
     </div>
     </>
   );
